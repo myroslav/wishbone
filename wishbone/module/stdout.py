e17184850c911ac24aff460f6b9a76394127b342
Jelle Smet <wishbone@smetj.net>
2017-09-15 23:17:10 +0200

    handle bulk events properly

--- wishbone/module/stdout.py
+++ wishbone/module/stdout.py
@@ -24,6 +24,7 @@
 from gevent import monkey; monkey.patch_sys(stdin=False, stdout=True, stderr=False)
 from wishbone.actor import Actor
 from wishbone.module import OutputModule
+from wishbone.event import extractBulkItems
 from os import getpid
 from colorama import init, Fore, Back, Style
 import sys
@@ -131,10 +132,7 @@ class STDOUT(OutputModule):
     def consume(self, event):
 
         if event.isBulk():
-            data = event.dumpFieldAsList(
-                event.kwargs.selection
-            )
-            data = "\n".join(data)
+            data = "\n".join([str(item) for item in extractBulkItems(event, self.kwargs.selection)])
         else:
             data = event.get(
                 event.kwargs.selection
